3c9405151ff3c880ebc544fbda7b09b7923a1a10
BartolomeyKant <notevill@gmail.com>
2024-01-28 16:57:13 +0500

    add tests

--- src/smart_house/devices.rs
+++ src/smart_house/devices.rs
@@ -27,7 +27,7 @@ impl Socket {
 impl SmartDevice for Socket {
     fn state_report(&self) -> String {
         format!(
-            "description: {}, current_power: {}, state: {}",
+            "description: {}, current power: {}, state: {}",
             self.description(),
             self.get_power(),
             if self.is_on {
@@ -56,9 +56,72 @@ impl Thermometr {
 impl SmartDevice for Thermometr {
     fn state_report(&self) -> String {
         format!(
-            "Thermometr: {} current value:{}",
+            "Thermometr: {} current value: {}",
             self.description(),
             self.value()
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::smart_house::SmartDevice;
+
+    use super::{Socket, Thermometr};
+
+    #[test]
+    fn socket_fields() {
+        let mut socket = Socket {
+            description: "socket 1".to_string(),
+            is_on: false,
+            current_pover: 12f32,
+        };
+
+        assert_eq!(socket.description(), "socket 1".to_string());
+        assert_eq!(socket.get_power(), 12f32);
+        assert!(!socket.is_on);
+        socket.turn_on();
+        assert!(socket.is_on);
+        socket.turn_off();
+        assert!(!socket.is_on);
+    }
+
+    #[test]
+    fn thermometr_fields() {
+        let thermometr = Thermometr {
+            description: "thermometr 1".to_string(),
+            value: 76f32,
+        };
+
+        assert_eq!(thermometr.description(), "thermometr 1".to_string());
+        assert_eq!(thermometr.value(), 76f32);
+    }
+
+    fn assert_device_report<T: SmartDevice>(device: &T, report: &str) {
+        assert_eq!(device.state_report().as_str(), report);
+    }
+
+    #[test]
+    fn socket_report() {
+        let socket = Socket {
+            description: "socket 2".to_string(),
+            is_on: true,
+            current_pover: 122f32,
+        };
+
+        assert_device_report(
+            &socket,
+            "description: socket 2, current power: 122, state: is on now",
+        );
+    }
+
+    #[test]
+    fn thermometr_report() {
+        let thermometr = Thermometr {
+            description: "thermometr 2".to_string(),
+            value: -235f32,
+        };
+
+        assert_device_report(&thermometr, "Thermometr: thermometr 2 current value: -235");
     }
 }
