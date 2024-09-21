d33c8e0cb6181cf01eaeb3795e53e6c68047e79d
BartolomeyKant <notevill@gmail.com>
2024-09-21 14:40:12 +0500

    deleted

--- README.md
+++
@@ -1,2 +0,0 @@
-# rustfp_hw_project
-Homework projects for otus rust for pro course
--- src/smart_house/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod interface;
-
-pub mod rooms;
-
-pub mod house;
-
-pub mod reports;
-
-pub use interface::*;
